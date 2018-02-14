2ff298cf7c363aec1222913b2b511d2b7ce39dc0
Arnold Bykov <arnold.bykov@gmail.com>
2018-02-14 17:40:09 -0500

    Adding core files.
    Adding core files.

---
+++ KentWind.Web/master.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title><?php echo $pagetitle; ?></title>
+	</head>
+
+	<body>
+		<div id="divHeader">
+			This is the header.
+		</div>
+		<div id="divLinks">
+			These are the links.
+		</div>
+		<div id="divBody">
+			// This is gonna be our main text for each page.
+		</div>
+		<div id="divFooter">
+			This is the footer.
+		</div>
+	</body>
+</html>
