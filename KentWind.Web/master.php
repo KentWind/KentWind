920d2f504b4934c19b074c3e0db8399a29f5af82
Spanglr <mspangl5@kent.edu>
2018-02-14 18:10:17 -0500

    Linked bootstrap to master.php
    Renamed bootstrap folder to "bootsrap" from "bootstrap-4.0.0-dist ".

--- KentWind.Web/master.php
+++ KentWind.Web/master.php
@@ -1,5 +1,13 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- bootstrap-4.0.0-dist -->
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+
 		<title><?php echo $pagetitle; ?></title>
 	</head>
 
@@ -11,10 +19,14 @@
 			These are the links.
 		</div>
 		<div id="divBody">
-			// This is gonna be our main text for each page.
+			This is gonna be our main text for each page:
+			<?php echo $pagecontents; ?>
 		</div>
 		<div id="divFooter">
 			This is the footer.
 		</div>
+
+		<script src="bootstrap/js/jquery-1.11.3.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
 	</body>
 </html>
