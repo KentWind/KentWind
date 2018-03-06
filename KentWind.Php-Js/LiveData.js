b407ef10b42c838fcd5b93f6e191275f4d894392
Logan <zenrion@gmail.com>
2018-03-06 15:01:07 -0500

    Added actual object vs variable to hold LiveData

--- KentWind.Php-Js/LiveData.js
+++ KentWind.Php-Js/LiveData.js
@@ -32,43 +32,57 @@
 //     setInterval(update, 1000);
 // });
 
-var liveData = getPHP();
-console.log("Outputting object after ajax call");
-console.log(liveData);
+
+/////////////////////////
+//  FUNCTIONS
+/////////////////////////
+
+function LiveData() {
+    this.windData;
+    var self = this;
+    this.output = function () {
+        console.log("\nLiveData output memberfunction:\n\n");
+        for (var i = 0; i < self.windData.length; i++) {
+            console.log("ROW " + i + ": ");
+            console.log("id: " + self.windData[i].id);
+            console.log("speed: " + self.windData[i].speed);
+            console.log("direction: " + self.windData[i].direction);
+            console.log("time: " + self.windData[i].time);
+            console.log("\n");
+        }
+    }
+}
 
 function getPHP() {
-    var data = { };
+    var data = new LiveData();
     $.ajax({
         url: 'query-test.php',
         type: 'POST',
         dataType: 'json',
         async: false,
         success: function (result) {
-            data = result;
+            data.windData = result;
         }
     });
     return data;
 }
 
-liveData.output = function () {
-    console.log("Outputting object's member variables after creating output function.");
-    for (var i = 0; i < this.length; i++) {
-        console.log("ROW " + i + ": ");
-        console.log("id: " + this[i].id);
-        console.log("speed: " + this[i].speed);
-        console.log("direction: " + this[i].direction);
-        console.log("time: " + this[i].time);
-        console.log("\n");
-    }
+function update() {
+    var newWindData = getPHP();
+    console.log(newWindData);
+    newWindData.output();
 }
 
-liveData.update = function () {
-    liveData = getPHP();
-    console.log(liveData);
-    setInterval(this.update, 1000);
-}
+/////////////////////////
+//  TESTING
+/////////////////////////
+
+var liveData = getPHP();
+console.log("Outputting object after ajax call");
+console.log(liveData);
+liveData.output();
 
 //When the entire web page has loaded, begin updating the live data.
 $(document).ready(function () {
-    liveData.update();
+    setInterval(update, 5000);
 });
