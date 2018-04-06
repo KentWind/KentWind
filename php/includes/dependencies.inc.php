6da7ddc7244cfabd5d67f229964dcc8f7445bc91
Arnold Bykov <arnold.bykov@gmail.com>
2018-04-06 01:39:15 -0400

    Beautifying About Page & Dependencies Fix
    - Made the About Us Page look better and hold more content.
    - Fixed dependencies errors.

--- php/includes/dependencies.inc.php
+++ php/includes/dependencies.inc.php
@@ -1,3 +1,9 @@
+<?php 
+	$BASE_URL = 'http://localhost/KentWind/';
+	$CSS = $BASE_URL . 'css/';
+	$JS = $BASE_URL . 'js/';
+?>
+
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -11,7 +17,7 @@
 <!-- bootstrap-3.3.7 CDN  +  jquery -->
 <!-- Latest compiled and minified CSS -->
 
-<link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" href="<?php echo $CSS . 'style.css' ?>" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 <!-- Optional theme -->
@@ -20,6 +26,6 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-<script src="js/windCanvas.js"></script>
+<script src="<?php echo $JS . 'windCanvas.js'?>"></script>
 
 <title>KentWind - <?php echo $pagetitle; ?></title>
