21461446e1ca8949121a95539a6c586e6b34189e
dbevila1 <dbevila1@kent.edu>
2018-04-25 18:02:18 -0400

    Fix Home page main content
    Fixed home page main content

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<?php
 		// Specify Page Title
 		$pagetitle = 'Home';
-		
+
 		// Dependencies Include
 		include 'php/includes/dependencies.inc.php';
 	?>
@@ -12,7 +12,7 @@
 
 	<!-- Navbar Includes -->
 	<?php include 'php/includes/navbar.inc.php'; ?>
-	
+
 	<!-- Page Specific Content -->
 	<ol class="breadcrumb">
 	  <li><a href="<?php echo $HOME; ?>">Home</a></li>
@@ -21,9 +21,10 @@
 	<div class="jumbotron">
 		<div class="container">
 			<h1>Welcome to KentWind&trade;</h1>
-			<p>Windmap solutions for literally the windiest campus in the United States of America. 
-			Couple that in with uncertain, but mostly cold Ohio climate and it's a challenge to avoid
-			getting hypothermia on your way to your 9AM Philosophy course they force you to take.</p>
+			<p>Kent State University Computer Science students designing a
+			system to measure and display the wind speeds and direction
+			within the Kent State University main campus.</p>
+			<p>Plan your route through the campus!</p>
 			<p><a class="btn btn-primary btn-lg" href="visualization.html">How windy is it?</a></p>
 		</div>
 	</div>
