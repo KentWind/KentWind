0e31949f8588fe896fb1040e3ced346a8165f931
Logan <zenrion@gmail.com>
2018-04-26 01:04:01 -0400

    Optimized serverPush.php

--- php/utilities/serverPush.php
+++ php/utilities/serverPush.php
@@ -10,17 +10,24 @@ if(isset($_POST['data'])) { //  If there is something to in the post superglobal
 
     $pdo = DBConnect::getInstance()->connect();
 
-    $moveToHistorical = 'INSERT INTO historical_data SELECT * FROM live_data WHERE SensorID = ' . $id;
-    $statement = $pdo->prepare($moveToHistorical);
-    $statement->execute();
+    $checkTableEmpty = 'SELECT 1 FROM live_data LIMIT 1;';   // Returns 1 if there is data in the table.
+    $statement = $pdo->prepare($checkTableEmpty);
+    $rowExists = $statement->execute();
 
-    $removeLiveData = 'DELETE FROM live_data WHERE SensorID = ' . $id;
-    $statement = $pdo->prepare($removeLiveData);
-    $statement->execute();
+    if ($rowExists == 0) { // If there is no data in the table, insert data into the table.
+        $insertNewData = 'INSERT INTO live_data (SensorID, Speed, Direction) VALUES (' . $id . ',' . $speed . ',' . $direction . ')';
+        $statement = $pdo->prepare($insertNewData);
+        $statement->execute();
+    }
+    else {  // If a data exists in the table, update the table.
+        $moveToHistorical = 'INSERT INTO historical_data SELECT * FROM live_data WHERE SensorID = ' . $id;
+        $statement = $pdo->prepare($moveToHistorical);
+        $statement->execute();
 
-    $insertNewData = 'INSERT INTO live_data (SensorID, Speed, Direction) VALUES (' . $id . ',' . $speed . ',' . $direction . ')';
-    $statement = $pdo->prepare($insertNewData);
-    $statement->execute();
+        $updateData = 'UPDATE live_data SET Speed =' . $speed . ', Direction = ' . $direction . ', Time = CURRENT_TIMESTAMP WHERE SensorID = ' . $id;
+        $statement = $pdo->prepare($updateData);
+        $statement->execute();
+    }
 
     $pdo.close();
 }
